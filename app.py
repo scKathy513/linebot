0c10dae9ff73886c20ec02e3d2c2317843310317
scKathy513 <43978523+scKathy513@users.noreply.github.com>
2018-10-24 22:04:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,10 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=(Ufd8167b43b2292b7097ede32a75675ac)) #reply userid
+    message = TextSendMessage(text='Ufd8167b43b2292b7097ede32a75675ac')
+    message2 = TextSendMessage(text=(event.source.user_id)) #reply userid
     line_bot_api.reply_message(event.reply_token, message)
+    print(event)
 
 import os
 if __name__ == "__main__":
